14401ae6135142dc67456efad155306eb1acb7c2
felix-lee123 <90741469+felix-lee123@users.noreply.github.com>
2024-03-22 17:55:28 +0800

    Update CustomLoginNavbarItem.js

--- Firmament-main/Firmament-main/src/components/NavbarItems/CustomLoginNavbarItem.js
+++ Firmament-main/Firmament-main/src/components/NavbarItems/CustomLoginNavbarItem.js
@@ -11,7 +11,7 @@ import 'firebase/compat/auth';
 
 // Configure Firebase.
 const config = {
-apiKey: "AIzaSyD8EcwY-enP40uMOK41581WZoflCPzU1sI",
+apiKey: ,
 authDomain: "firmament-a19df.firebaseapp.com",
 databaseURL: "https://firmament-a19df-default-rtdb.asia-southeast1.firebasedatabase.app",
 projectId: "firmament-a19df",
